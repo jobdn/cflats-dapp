c106119dcf37b8447e8ef1a55b2b5b52261ca15f
Dan Pisarev <danila.pisarev@facecast.net>
2024-04-13 15:07:07 +0300

    feat: add land component

---
+++ src/shared/ui/Land/Land.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import clsx from "clsx";
+
+import { GenNumber } from "@/shared/types";
+
+import key from "./images/key.png";
+
+import classes from "./Land.module.scss";
+
+type LandProps = {
+  type: "unlocked" | "locked";
+  title?: string;
+  name: string;
+  gen: GenNumber;
+  className?: string;
+};
+
+export const Land = (props: LandProps) => {
+  const { gen, name, type, className, title } = props;
+
+  return (
+    <div
+      className={clsx(
+        classes.land,
+        classes[`land_${type}`],
+        type === "unlocked" && classes[`land_gen${gen}`],
+        className
+      )}
+    >
+      <h3 className={classes.title}>{title || type}</h3>
+      <Image src={key} alt="Land icon" />
+
+      <div className={classes.desc}>
+        <p>GEN#{gen}</p>
+        <p className={classes.name}>{name}</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/shared/ui/Land/index.ts
@@ -0,0 +1,1 @@
+export { Land } from "./Land";
